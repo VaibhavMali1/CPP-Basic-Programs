b5e2c8f06de6ee57eed29d5ebe839598b1196f1f
VaibhavMali1 <malivaibhav311@gmail.com>
2022-11-16 18:37:15 +0530

    [ADDED]:The Program of Find The Area of Triangle

---
+++ BasicP5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace::std;
+int main()
+{
+	float area,base=0,height=0;
+	cout<<"Enter the Base:";
+	cin>>base;
+	cout<<"\nEnter the Height:";
+	cin>>height;
+	area=0.5*base*height;
+	cout<<"\nArea of Triangle:"<<area;
+}
