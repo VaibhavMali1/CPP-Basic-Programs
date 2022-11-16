4f5c0782d0542a79b3f9e16a5b7f1fc2bd047851
VaibhavMali1 <malivaibhav311@gmail.com>
2022-11-16 18:35:18 +0530

    [ADDED]:The Program of Find The Area of Circle

---
+++ BasicP4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace::std;
+int main()
+{
+	float area,pi=3.14,r=0;
+	cout<<"Enter the Radius of Circle:";
+	cin>>r;
+	area=pi*r*r;
+	cout<<"\nArea of Circle="<<area;
+}
