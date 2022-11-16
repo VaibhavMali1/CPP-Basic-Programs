7405215e6b19ad49ce916bc770fc39be18bfd3f0
VaibhavMali1 <malivaibhav311@gmail.com>
2022-11-16 18:33:21 +0530

    [ADDED]:The Program of Increment or Decrement Operator

---
+++ BasicP3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace::std;
+int main()
+{
+	int a,b;
+	cout<<"\nEnter the Number of A :";
+	cin>>a;
+	cout<<"\nEnter the Number of B :";
+	cin>>b;
+	a++;
+	cout<<"\nIncrement of A:"<<a++;
+	b--;
+	cout<<"\nDecrement of B:"<<b--;
+	return 0;
+}
