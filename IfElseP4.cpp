18e12bfaa3122ace69855b1e3b31329a6a71e710
VaibhavMali1 <malivaibhav311@gmail.com>
2022-11-17 16:31:26 +0530

    [ADDED]:The Program of Check if the Value Match the Value of Variable by Using The If Else

---
+++ IfElseP4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace::std;
+ int main () {
+	int a = 100;
+   if( a == 10 ) {
+      cout<<"Value of a is 10\n";
+   } else if( a == 20 ) 
+   {
+      cout<<"Value of a is 20\n";
+   } else if( a == 30 ) {
+      cout<<"Value of a is 30\n";
+   } else {
+      cout<<"None of the values is matching\n";
+   }
+    cout<<"Exact value of a is:\n"<<a;
+   return 0;
+}
