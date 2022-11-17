b5e7b4ddb4356e7b14d7c66d8e94a33af73a8abe
VaibhavMali1 <malivaibhav311@gmail.com>
2022-11-17 16:34:27 +0530

    [ADDED]:The Program of Check the Number is Odd or Even by Using The If Else

---
+++ IfElseP5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace::std;
+int main (){
+   int a=4;
+   cout<<"Enter the value of a:";
+   cin>>a;
+   if(a%2==1){
+      cout<<"a is odd number";
+   }else{
+      cout<<"a is even number";
+   }
+   return 0;
+}
