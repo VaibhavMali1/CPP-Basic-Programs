4a5e88835a6dcebce826a1e232b2f4e63745d854
VaibhavMali1 <malivaibhav311@gmail.com>
2022-11-19 17:45:52 +0530

    [ADDED]:The Program of Know the Actual Programming Language with the Help of Switch Case

---
+++ SCaseP4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace::std;
+int main() {
+int language = 10;
+  switch (language) {
+  case 1:
+    cout<<"C#\n";
+    break;
+  case 2:
+    cout<<"C\n";
+    break;
+  case 3:
+    cout<<"C++\n";
+    break;
+  default:
+    cout<<"Other programming language\n";
+	}
+	return 0;
+}
