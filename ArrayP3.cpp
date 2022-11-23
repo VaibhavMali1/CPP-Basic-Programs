08f515ecce9f404cb20385c88d9f806bd8f1c6fc
VaibhavMali1 <malivaibhav311@gmail.com>
2022-11-23 11:41:42 +0530

    [ADDED]:The Program of Check the Matrix is Diagonal Matrix or Non-Diagonal Matrix by Using the Array

---
+++ ArrayP3.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace::std;
+int main()
+{
+	int a[3][3],r,c,flag=1;
+	cout<<"Enter the 3x3 Matrix:";
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		{
+			cin>>a[r][c];
+		}
+	}
+	cout<<"\nThe 3x3 Matrix is :\n";
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		{
+			cout<<"\t"<<a[r][c];
+		}
+		cout<<"\n";
+	}
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		{
+			if(r==c && a[r][c]!=0)
+			{
+				flag=0;
+				break;
+			}
+		}
+	}
+	if(flag==1)
+	{
+		cout<<"\nThe 3x3 Matrix is Non-Diagonal Matrix";
+	}
+	else
+	{
+		cout<<"\nThe 3x3 matrix is Diagonal Matrix";
+	}
+	return 0;
+}
