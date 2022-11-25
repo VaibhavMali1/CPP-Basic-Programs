1c07bf7d2ea3675f9732e0a6f7ac4e83a967555f
VaibhavMali1 <malivaibhav311@gmail.com>
2022-11-25 12:06:45 +0530

    [ADDED]:The Program of Find the Maximum Value by Comparing Two Numbers by Using the Functions

---
+++ FunctionsP8.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace::std;
+int max(int num1, int num2);
+int main () {
+   int a = 500;
+   int b = 200;
+   int ret;
+   ret = max(a, b);
+   cout << "Max value is : " << ret << endl;
+   return 0;
+}
+int max(int num1, int num2) {
+    int result;
+   if (num1 > num2)
+      result = num1;
+   else
+      result = num2;
+   return result; 
+}
