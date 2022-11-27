340e2dd55a865fbc6320fc0abf503a38cdeb59f7
VaibhavMali1 <malivaibhav311@gmail.com>
2022-11-27 22:20:13 +0530

    [ADDED]:The Program of the Function of String Header File gets and puts to take input from User and Display the String respectively

---
+++ SF9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace::std;
+#include <string.h>
+int main()
+{
+
+char temp[20];
+
+cout<<"Enter your Name:";
+
+gets(temp);
+
+cout<<"My Name is:";
+
+puts(temp);
+
+return 0;
+
+}
