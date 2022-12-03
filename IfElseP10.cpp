7885f104e306f4d3d84e949a5bfd8c12f983e6e0
VaibhavMali1 <malivaibhav311@gmail.com>
2022-12-03 20:14:17 +0530

    [ADDED]:The Program of Find the Statement is True or Not by Comparing the Numbers with the Help of If Else

---
+++ IfElseP10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace::std;
+
+int main(void) {
+    int n = 2;
+
+    if(n == 3) { 
+        cout<<"Statement is True!\n";
+    } 
+    else { 
+        cout<<"Statement is False!\n";
+    }
+
+    return 0;
+}
