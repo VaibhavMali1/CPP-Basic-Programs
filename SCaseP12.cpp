193c3f718e442d0040cec972632b43d8bff50a1d
VaibhavMali1 <malivaibhav311@gmail.com>
2022-12-10 13:49:15 +0530

    [ADDED]:The Program of Valid Case Values in Switch.

---
+++ SCaseP12.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace::std;
+int main()
+{
+	switch (-2) {
+	case 2-4:
+		cout<<"I am -2";
+        break;
+ 	case 2:
+         cout<<"I am 2";
+         break;
+ 	case 10/7:
+         cout<<"I am 1";
+         break;
+	default:
+         cout<<"I am default";
+         break;
+}
+
+}
