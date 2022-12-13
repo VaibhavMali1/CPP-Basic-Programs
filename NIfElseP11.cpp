254c4c95384a0c84c6516c0c77f16f48e74fa373
VaibhavMali1 <malivaibhav311@gmail.com>
2022-12-13 12:48:32 +0530

    [ADDED]:The Program of Check the Marks if the Student Pass or Fail by using the Nested If Else.

---
+++ NIfElseP11.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace::std;
+int main()
+{
+	int mark = 100;
+
+	if (mark >= 50) {
+	cout << "You passed." << endl;
+	if (mark == 100) {
+    cout <<"Perfect!" << endl;
+  }
+	}
+	else {
+	cout << "You failed." << endl;
+	}
+}
