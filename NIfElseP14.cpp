2df12eec74021ae9b2ede25a365e848753af2bb9
VaibhavMali1 <malivaibhav311@gmail.com>
2022-12-13 12:54:43 +0530

    [ADDED]:The Program of Compare the Values of Variables by Using the Nested If Else.

---
+++ NIfElseP14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace::std;
+int main()
+{
+	int a = 10;
+	int b = 20;
+	if (a == 10) { 
+	cout << "value of a is: " << a << endl;
+	if ( b == 20) { 
+	cout << " value of b is: " << b << endl;
+	}
+	}
+	cout << " End of program... " << endl;
+return 0;
+}
