b0d9b229a3aeb22ac6f94ec352c2a7c965a71557
VaibhavMali1 <malivaibhav311@gmail.com>
2022-12-19 18:47:51 +0530

    [ADDED]:The Program to Print the Value from a Specific Point to a Specific Point by Using the While Loop.

---
+++ WhileLoopP13.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace::std;
+int main () 
+{
+  int a;
+  cout<<"enter any digit"; 
+  cin>>a;
+  while (a>=1 &&a <=10) 
+  {
+    cout<<"\n the value of a is "<< a; 
+    a++;
+  } 
+   return 0;
+}
