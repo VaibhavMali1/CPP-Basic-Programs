8346b1cdfab338568d6c0fde2c7df191402f2715
VaibhavMali1 <malivaibhav311@gmail.com>
2022-12-21 12:41:39 +0530

    [ADDED]:The Program made by Using Three String Variables.

---
+++ SF12.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstring>
+using namespace::std;
+int main(){
+
+	string str1 = "ice"; 
+	string str2 = "cream"; 
+	string str3; 
+	str3 = str1 + str2; 
+	cout << str3;
+	return 0;
+}
