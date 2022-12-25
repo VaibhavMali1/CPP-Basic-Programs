58e0b16b87150cc2f8b7f79ee8334f7f79d9cf63
VaibhavMali1 <malivaibhav311@gmail.com>
2022-12-25 18:53:49 +0530

    [ADDED]:The Program of Using the Break Statement Example to Exit the Loop.

---
+++ ForLoopP18.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace::std;
+int main() {
+ 
+	for (int x = 1; x <= 10; ++x) {
+ 
+ 	 if (x == 6) {
+ 
+   	 break;
+ 
+  	}
+ 
+     cout << "Current value of x= " << x << endl;
+ 
+  }
+ 
+  	return 0;
+ 
+}
