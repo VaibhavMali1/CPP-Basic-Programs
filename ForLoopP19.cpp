74e6a1a6e0b04e34ae5eb1399e972f02bbfa2c5d
VaibhavMali1 <malivaibhav311@gmail.com>
2022-12-25 18:55:19 +0530

    [ADDED]:The Program of Iterating Through Array Example by Using the For Loop.

---
+++ ForLoopP19.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace::std;
+int main() {
+ 
+	string fruits[5] = {"Apple", "Banana", "Mango", "Orange", "Watermelon"};
+ 
+  	for (int x = 0; x < 5; x++) {
+ 
+    cout << fruits[x] << "\n";
+ 
+  }
+ 
+  	return 0;
+ 
+}
