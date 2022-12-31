bae7add390d2607752292d04072a855e035c19b7
VaibhavMali1 <malivaibhav311@gmail.com>
2022-12-31 19:13:22 +0530

    [ADDED]:The Program to Implement Function Wide Character and Get the Size of It .

---
+++ SF16.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace::std;
+int main()
+{
+
+	wchar_t c = L'S' ;
+
+	cout << "The wide character value 'S' is: " << c << endl ;
+	cout << "Wide character size is " << sizeof(c) ;
+	return 0;
+}
