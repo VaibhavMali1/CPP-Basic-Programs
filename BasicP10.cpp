df38be3754e356f2b7d9717357628b23b15577e6
VaibhavMali1 <malivaibhav311@gmail.com>
2023-01-01 18:35:35 +0530

    [ADDED]:The Program to Display the ASCII Values.

---
+++ BasicP10.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace::std;
+int main()
+{
+	int num;
+	char chara;
+	cout<<"enter character:";
+	cin>>chara;
+	num=chara;
+	cout<<"ASCII value of:"<<chara<<"="<<num;
+	
+}
