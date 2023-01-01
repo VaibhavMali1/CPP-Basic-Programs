0dd80f7528330aa92aafc87d211fc31d319fa84a
VaibhavMali1 <malivaibhav311@gmail.com>
2023-01-01 18:33:37 +0530

    [ADDED]:The Program to Find the Time, Distance and Speed.

---
+++ BasicP8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace::std;
+int main()
+{
+	int dis,sp;
+	float t;
+	cout<<"Enter the distance: ";
+	cin>>dis;
+	cout<<"Enter the speed: ";
+	cin>>sp;
+	t=dis/sp;
+	cout<<"The time required is: "<<t<<endl;
+}
